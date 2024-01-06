2b74906367372478d9e6db8c9b728a4cffdcdc79
pilot-max <max.udaskin@gmail.com>
2024-01-06 10:34:24 -0500

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Max Udaskin on 2024-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Max Udaskin on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
